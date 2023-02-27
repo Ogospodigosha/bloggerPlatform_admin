acbad20b052e4f210a1ca46fcf0ba2d9b84f5e9e
Ogospodigosha <odintsovis@hotmail.com>
2023-02-28 00:22:55 +0300

    add: formik

--- src/features/blogs/addBlogPage/addBlogContent/AddBlogContent.tsx
+++ src/features/blogs/addBlogPage/addBlogContent/AddBlogContent.tsx
@@ -1,9 +1,18 @@
 import { Box, Breadcrumbs, Button, Divider,  Link, TextField, Toolbar } from '@mui/material';
-
+import { Formik } from 'formik';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from "./AddBlogContent.module.css";
 import WestIcon from '@mui/icons-material/West';
+
+
+
+export type FormikErrorType = {
+    name?: string
+    description?: string
+    websiteUrl?: string
+}
+
 
 export const AddBlogContent = () => {
     const addBlog = ()=>{
@@ -28,16 +37,98 @@ export const AddBlogContent = () => {
                 </div>
             </NavLink>
             <div style={{width: '100%', height: '312px', backgroundColor: 'white', marginBottom: '24px'}}></div>
-            <TextField id="standard-basic" label="Blog Name" variant="standard" fullWidth style={{marginBottom: "24px"}}/>
-            <TextField id="standard-basic" label="Website" variant="standard" fullWidth style={{marginBottom: "54px"}}/>
-            <textarea style={{backgroundColor: 'inherit', width: '100%', height:"120px", marginBottom: '24px', marginRight: '64px'}}></textarea>
-            <div style={{display: 'flex', justifyContent:'flex-end'}}>
-                <Button variant={'contained'} sx={{
-                    marginRight: '64px',
-                    "&.MuiButton-containedPrimary": {backgroundColor: '#F8346B'},
-                    marginBottom: '29px'
-                }} onClick={addBlog}>Add blog</Button>
-            </div>
+            {/*<TextField id="standard-basic" label="Blog Name" variant="standard" fullWidth style={{marginBottom: "24px"}}/>*/}
+            {/*<TextField id="standard-basic" label="Website" variant="standard" fullWidth style={{marginBottom: "54px"}}/>*/}
+            {/*/!*<textarea style={{backgroundColor: 'inherit', width: '100%', height:"120px", marginBottom: '24px', marginRight: '64px'}}></textarea>*!/*/}
+            {/*<div style={{display: 'flex', justifyContent:'flex-end'}}>*/}
+            {/*    <Button variant={'contained'} sx={{*/}
+            {/*        marginRight: '64px',*/}
+            {/*        "&.MuiButton-containedPrimary": {backgroundColor: '#F8346B'},*/}
+            {/*        marginBottom: '29px'*/}
+            {/*    }} onClick={addBlog}>Add blog</Button>*/}
+            {/*</div>*/}
+           <Formik initialValues={{name: '', description:'', websiteUrl: ''}}
+                   validate={values => {
+                       const errors: FormikErrorType = {};
+                       if (!values.name) {
+                           errors.name = 'Required';
+                       }
+                       if (!values.description) {
+                           errors.description = 'Required';
+                       }
+                       if (!values.websiteUrl) {
+                           errors.websiteUrl = 'Required'
+                       }
+                       // else if ()
+                       //     errors.websiteUrl = 'websiteUrl must be a URL address'
+                        return errors
+                   }}
+                   onSubmit={(values, actions) => {
+                       debugger
+                       if (values.name && values.websiteUrl && values.description)
+                           // dispatch(registrationTC(values.email, values.password))
+                           console.log('123123')
+                   }}
+           >
+               {({
+                     values,
+                     errors,
+                     touched,
+                     handleChange,
+                     handleBlur,
+                     handleSubmit,
+                     isSubmitting,
+                     /* and other goodies */
+                 }) => (
+                   <form onSubmit={handleSubmit}>
+                       <TextField
+                           id="standard-basic"
+                           label="Blog Name"
+                           variant="standard"
+                           fullWidth
+                           type="text"
+                           name="name"
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.name}
+                           style={{marginBottom: "24px"}}
+                       />
+                       {errors.name && touched.name && errors.name}
+                       <TextField
+                           style={{marginBottom: "54px"}}
+                           id="standard-basic"
+                           label="Website"
+                           variant="standard"
+                           fullWidth
+                           type="text"
+                           name="websiteUrl"
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.websiteUrl}
+                       />
+                       {errors.websiteUrl && touched.websiteUrl && errors.websiteUrl}
+                       <TextField
+                           label="description"
+                           type="text"
+                           name="description"
+                           fullWidth
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           value={values.description}
+                           style={{marginBottom: '24px'}}
+                       />
+                       {errors.description && touched.description && errors.description}
+
+                       <div style={{display: 'flex', justifyContent:'flex-end'}}>
+                           <Button variant={'contained'} sx={{
+                               marginRight: '64px',
+                               "&.MuiButton-containedPrimary": {backgroundColor: '#F8346B'},
+                               marginBottom: '29px'
+                           }} onClick={addBlog} type={'submit'}>Add blog</Button>
+                       </div>
+                   </form>
+               )}
+           </Formik>
         </Box>
     );
 };
