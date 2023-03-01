662b3444287c9a969f7e49f971e15d532c3c87d7
Ogospodigosha <odintsovis@hotmail.com>
2023-03-01 15:43:49 +0300

    add: app-reducer

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,26 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: null as RequestErrorType,
+
+}
+
+
+
+export const slice = createSlice({
+    name: 'app',
+    initialState: initialState,
+    reducers: {
+        SetAppStatus(state, action: PayloadAction<{status: RequestStatusType}>){
+            state.status = action.payload.status
+        },
+        SetAppError(state, action: PayloadAction<{error: RequestErrorType}>) {
+            state.error = action.payload.error
+        }
+    }
+})
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type RequestErrorType = null | string
